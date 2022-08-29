5b9a3aa6da68810ff48a4c4bdafa74a71c70acf3
jandyra <jandyrapearcebello@gmail.com>
2022-08-29 14:20:21 -0300

    AmmalA-jandyra-pearce

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+// let nome 
+// let idade
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+//Reflexao 
+//undefined pois é uma variavel indefinida
+
+// let nome = prompt("Qual o seu nome? ")
+// let idade = prompt("Quantos anos você tem? ")
+
+// console.log(typeof nome, nome)
+// console.log(typeof idade, idade)
+
+//Reflexao
+//objeto null pois tem uma variavel definida
+
+let nome = prompt("Qual seu nome? ")
+let idade = prompt("Quantos anos você tem?")
+
+console.log("Olá", nome, "voce tem", idade, "anos.")
+
